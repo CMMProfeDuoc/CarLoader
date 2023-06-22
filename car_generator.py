9c2d3bf57ff37ab383ac9a9e217ad497de567010
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-21 23:20:28 -0400

    funcionaaaa

--- car_generator.py
+++ car_generator.py
@@ -2,36 +2,44 @@ from file_loader import *
 import random
 from random import randint
 
-maxCarCount = 10
-
 colores = ["Rojo","Verde","Azul","Amarillo","Morado","Negro","Blanco","Burdeo"]
 carFiles = [["Ferrari"],["Honda"],["Toyota"],["Mitsubishi"]]
-
-patentes = generador_patentes(maxCarCount)
-
-autos = []
-
-random.seed()
 
 for car in carFiles:
 	lista = load_file(car[0])
 	for l in lista:
 		car.append(l)
 
-#print("TEST",carFiles[0][1][1])
+#poco eficaz, pero más visible
+files = []
+files.append(["Autos1",20])
+files.append(["Autos2",18])
+files.append(["Autos3",25])
+files.append(["AutosBIG1",100])
+files.append(["AutosBIG2",150])
+files.append(["AutosTEST1",100])
+files.append(["AutosTEST2",200])
 
-for pat in patentes:
-	m = randint(0,len(carFiles)-1)
-	marca = carFiles[m][0]
-	n = randint(1,len(carFiles[m])-1)
-	modelo = carFiles[m][n][0]
 
-	año = randint(int(carFiles[m][n][1]),int(carFiles[m][n][2]))
+for f in files:
+	patentes = generador_patentes(f[1])
+	autos = []
+	random.seed()
+	#print("TEST",carFiles[0][1][1])
 
-	color = colores[randint(0,len(colores)-1)]
+	for pat in patentes:
+		m = randint(0,len(carFiles)-1)
+		marca = str(carFiles[m][0])
+		n = randint(1,len(carFiles[m])-1)
+		modelo = str(carFiles[m][n][0])
 
-	#print(marca,modelo,año,pat,color)
+		año = str(randint(int(carFiles[m][n][1]),int(carFiles[m][n][2])))
 
-	autos.append({"marca":marca,"modelo":modelo,"año":año,"patente":patente,"color":color})
+		color = str(colores[randint(0,len(colores)-1)])
 
-print(autos)
+		#print(marca,modelo,año,pat,color)
+		#autos.append({"marca":marca,"modelo":modelo,"año":año,"patente":pat,"color":color})
+		line = marca + ";" + modelo + ";" + año + ";" + pat + ";" + color
+		autos.append(line)
+
+	generador_archivo(autos,f[0])
