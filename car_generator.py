9c6fa96adddb47615af8d91afef8cc8e59b3ba7f
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-21 23:07:05 -0400

    funciona

--- car_generator.py
+++ car_generator.py
@@ -2,10 +2,15 @@ from file_loader import *
 import random
 from random import randint
 
+maxCarCount = 10
+
 colores = ["Rojo","Verde","Azul","Amarillo","Morado","Negro","Blanco","Burdeo"]
 carFiles = [["Ferrari"],["Honda"],["Toyota"],["Mitsubishi"]]
 
-carCount = 100
+patentes = generador_patentes(maxCarCount)
+
+autos = []
+
 random.seed()
 
 for car in carFiles:
@@ -13,6 +18,20 @@ for car in carFiles:
 	for l in lista:
 		car.append(l)
 
-print(carFiles[2])
+#print("TEST",carFiles[0][1][1])
 
+for pat in patentes:
+	m = randint(0,len(carFiles)-1)
+	marca = carFiles[m][0]
+	n = randint(1,len(carFiles[m])-1)
+	modelo = carFiles[m][n][0]
 
+	año = randint(int(carFiles[m][n][1]),int(carFiles[m][n][2]))
+
+	color = colores[randint(0,len(colores)-1)]
+
+	#print(marca,modelo,año,pat,color)
+
+	autos.append({"marca":marca,"modelo":modelo,"año":año,"patente":patente,"color":color})
+
+print(autos)
